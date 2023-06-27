7e034be62adf670e20ed6068e2b28ed08453755c
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-06-27 17:19:13 +0530

    tried getElementById and querySelector

--- javascript_tutorials/app.js
+++ javascript_tutorials/app.js
@@ -1,1 +1,11 @@
 console.log("This is from external file")
+
+
+setTimeout(() => {
+    console.log(document.getElementById("fav_games"))
+    console.log(document.getElementById("popular_games"))
+    console.log(document.querySelector(".fav"))
+    console.log(document.querySelectorAll(".card"))
+    console.log(document.querySelectorAll(".fav"))
+    console.log(document.querySelector(".card"))
+}, 1000)
