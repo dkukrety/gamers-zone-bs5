8b7266e01446347bf5679fde6a0f408efb76dfba
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-06-28 17:59:33 +0530

    changes

--- javascript_tutorials/app.js
+++ javascript_tutorials/app.js
@@ -7,3 +7,29 @@ console.log('Hello, ' + name); // Logs a message with the entered name
 
 const result = confirm('Are you sure you want to learn full stack development?');
 console.log(result)
+
+
+
+var age = 23;
+var place = "Bengaluru";
+const designation = null;
+var company;
+var voter = true;
+
+const fruits = ["apple", "mango", "pineapple", "banana"];
+const student = {
+    name: "Deepak",
+    "age": 15,
+    company: "Intel",
+    designation: "Intern",
+    salary: 30000
+};
+
+
+function order() {
+    console.log("This is order")
+}
+
+order();
+
+
