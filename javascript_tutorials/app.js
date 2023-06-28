26bb697c16ca405ae22080f55162ce7f9668550f
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-06-28 16:21:35 +0530

    primitive data types

--- javascript_tutorials/app.js
+++ javascript_tutorials/app.js
@@ -1,16 +1,31 @@
-console.log("This is from external file")
+// console.log("This is from external file")
+
+
+var student1 = "Rakshit";
+var age = 21;
+var eligibleForVoting = true;// true or false
+var job = null;
+var designation;
+var company = Symbol("ExcelR")
+console.log(student1 + " " + age + " voting eligibility " + eligibleForVoting);
+console.log(job)
+console.log(designation)
+console.log(company)
+
+
 
 setTimeout(() => {
-    console.log(document.getElementById("fav_games"))
-    console.log(document.getElementById("popular_games"))
-    console.log(document.querySelector(".fav"))
-    console.log(document.querySelectorAll(".card"))
-    console.log(document.querySelectorAll(".fav"))
-    console.log(document.querySelector(".card"))
-    console.log(document.getElementsByClassName("popular_game"));
+    // console.log(document.getElementById("fav_games"))
+    // console.log(document.getElementById("popular_games"))
+    // console.log(document.querySelector(".fav"))
+    // console.log(document.querySelectorAll(".card"))
+    // console.log(document.querySelectorAll(".fav"))
+    // console.log(document.querySelector(".card"))
+    // console.log(document.getElementsByClassName("popular_game"));
 
-    console.log(document.getElementsByTagName("section"));
-    document.title = "Spartans"
+    // console.log(document.getElementsByTagName("section"));
+    // document.title = "Spartans"
 
-    document.getElementById("fav_games").style.backgroundColor = "red";
+    // document.getElementById("fav_games").style.backgroundColor = "red";
 }, 1000)
+
