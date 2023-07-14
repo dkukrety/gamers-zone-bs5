395e74b694ef7790049e1c25261c706c5906dfbb
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-07-14 18:11:04 +0530

    Mixin

---
+++ src/mixin/mixin.js
@@ -0,0 +1,95 @@
+// Mixin for user authentication functionality
+const AuthenticationMixin = {
+    isAuthenticated: false,
+
+    login(username, password) {
+        // Simulate authentication logic
+        if (username === 'admin' && password === 'password') {
+            this.isAuthenticated = true;
+            console.log('User logged in successfully.');
+        } else {
+            console.log('Invalid credentials. Please try again.');
+        }
+    },
+
+    logout() {
+        // Simulate logout logic
+        this.isAuthenticated = false;
+        console.log('User logged out successfully.');
+    },
+
+    // Additional methods and properties related to authentication...
+};
+
+// Class using the AuthenticationMixin
+class UserService {
+    static withAuthentication = AuthenticationMixin;
+
+    // Other methods and properties specific to the UserService class...
+}
+
+// Using the class with the AuthenticationMixin
+const authService = new UserService.withAuthentication();
+authService.login('admin', 'password'); // Output: User logged in successfully.
+console.log(authService.isAuthenticated); // Output: true
+
+authService.logout(); // Output: User logged out successfully.
+console.log(authService.isAuthenticated); // Output: false
+
+// Mixin for data caching functionality
+const DataCachingMixin = {
+    cache: {},
+
+    cacheData(key, data) {
+        this.cache[key] = data;
+        console.log(`Data cached with key '${key}'.`);
+    },
+
+    retrieveCachedData(key) {
+        const cachedData = this.cache[key];
+        if (cachedData) {
+            console.log(`Retrieved cached data with key '${key}'.`);
+            return cachedData;
+        } else {
+            console.log(`No data found for key '${key}'.`);
+            return null;
+        }
+    },
+
+    // Additional methods and properties related to data caching...
+};
+
+
+
+
+// Class using mixins
+class DataService {
+    // Use the AuthenticationMixin
+    static withAuthentication = AuthenticationMixin;
+
+    // Use the DataCachingMixin
+    static withDataCaching = DataCachingMixin;
+
+    // Other methods and properties specific to the DataService class...
+}
+
+// Using the class with the DataCachingMixin
+const dataService = new DataService.withDataCaching();
+dataService.cacheData('user', { name: 'John', age: 30 }); // Output: Data cached with key 'user'.
+
+const cachedUser = dataService.retrieveCachedData('user');
+console.log(cachedUser); // Output: { name: 'John', age: 30 }
+
+const nonExistentData = dataService.retrieveCachedData('nonexistent');
+console.log(nonExistentData); // Output: No data found for key 'nonexistent'.
+// Using the class with mixins
+const AuthenticatedDataService = DataService.withAuthentication;
+const CachingDataService = DataService.withDataCaching;
+
+const service1 = new AuthenticatedDataService();
+service1.login('username', 'password');
+service1.logout();
+
+const service2 = new CachingDataService();
+service2.cacheData('key', 'data');
+const cachedData = service2.retrieveCachedData('key');
